c5c9aa1edc73401ab6bdf21ced1cf6d117c015dc
vignesh <vignesh.bsk@gmail.com>
2021-08-14 00:31:49 +0530

    added background image

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 const Login = (props) => {
     return(
         <Container>
-            <Content>Content</Content>
+            <Content>
+                <BgImage/>
+            </Content>
         </Container>
     );
 };
@@ -11,8 +13,36 @@ const Login = (props) => {
 const Container = styled.section`
     Overflow:hidden;
     display:flex;
+    flex-direction:column;
+    text-align:center;
+    height:100vh;
 `;
-const Content = styled.div``;
 
+const Content = styled.div`
+    margin-bottom: 10vw;
+    width:100%;
+    position:relative;
+    min-height:100vh;
+    box-sizing:border-box;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    padding: 80px 40px;
+    height: 100%;
+`;
+
+const BgImage = styled.div`
+    height: 100%;
+    background-position:top;
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-image:url("/images/login-background.jpg");
+    position:absolute;
+    top:0;
+    right:0;
+    left:0;
+    z-index:-1;
+`;
 
 export default Login;
